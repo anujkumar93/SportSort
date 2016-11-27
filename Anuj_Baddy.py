3376d6c2083ec2228d3ef94e54e29810a04115a3
anujkumar93 <anujkumar93@users.noreply.github.com>
2016-11-27 03:07:04 -0500

    Updating file
    The images are visible now, and the final data is stored in finalMatrix variable

--- Anuj_Baddy.py
+++ Anuj_Baddy.py
@@ -6,7 +6,7 @@ import time
 import matplotlib.pyplot as plt
 %matplotlib inline
 
-fileName='./ML project/Badminton_person2.csv'
+fileName='./ML project/Basketball_Anuj2.csv'
 data=np.loadtxt(fileName,dtype='string', delimiter=', ')
 counter=1;
 firstSensor="Accelerometer"
@@ -58,17 +58,17 @@ secondSensor_2=data[counter:,3]
 secondSensor_3=data[counter:,4]
 
 fft_firstSensor1=scipy.fftpack.fft(firstSensor_1)
-# fft_firstSensor1[0:1]=0
+fft_firstSensor1[0:1]=0
 fft_firstSensor2=scipy.fftpack.fft(firstSensor_2)
-# fft_firstSensor2[0:1]=0
+fft_firstSensor2[0:1]=0
 fft_firstSensor3=scipy.fftpack.fft(firstSensor_3)
-# fft_firstSensor3[0:1]=0
+fft_firstSensor3[0:1]=0
 fft_secondSensor1=scipy.fftpack.fft(secondSensor_1)
-# fft_secondSensor1[0:1]=0
+fft_secondSensor1[0:1]=0
 fft_secondSensor2=scipy.fftpack.fft(secondSensor_2)
-# fft_secondSensor2[0:1]=0
+fft_secondSensor2[0:1]=0
 fft_secondSensor3=scipy.fftpack.fft(secondSensor_3)
-# fft_secondSensor3[0:1]=0
+fft_secondSensor3[0:1]=0
 
 x_firstSensor1=np.linspace(0,25,fft_firstSensor1.size/2)
 x_firstSensor2=np.linspace(0,25,fft_firstSensor2.size/2)
@@ -77,21 +77,31 @@ x_secondSensor1=np.linspace(0,25,fft_secondSensor1.size/2)
 x_secondSensor2=np.linspace(0,25,fft_secondSensor2.size/2)
 x_secondSensor3=np.linspace(0,25,fft_secondSensor3.size/2)
 
-fftWidth=5 #no of seconds to take FFT over
+fftWidth=7 #no of seconds to take FFT over
 fftJump=1 #no of seconds to jump to take next fft
-numColumns=26 #total no of columns to include in one image
+numColumns=54 #total no of columns to include in one image
 numSecondsPerImage=fftJump*(numColumns-1)+fftWidth #make sure this divides 120 exactly (since concatenated file has 2 min)
 
 #6 is the number of sensors
-finalMatrix=np.zeros(((50*fftWidth)/2),numColumns,6,totalNumOfLines/(50*2*numSecondsPerImage))
+finalMatrix=np.zeros((((50*fftWidth)/2),numColumns,6,totalNumOfLines/(50*2*numSecondsPerImage)))
 for n in range(finalMatrix.shape[3]):
     for k in range(3):
         for j in range(finalMatrix.shape[1]):
-            currFirstSensor=data[n*1750+250*j:counter,k+2]
-            currSecondSensor=data[counter:,k+2]
-            
+            currFirstSensor=data[(n*50*numSecondsPerImage)+(j*50*fftJump):\
+                                 (n*50*numSecondsPerImage)+((j*50*fftJump)+(50*fftWidth)),k+2]
+            curr_fft_firstSensor=scipy.fftpack.fft(currFirstSensor)
+            currSecondSensor=data[counter+(n*50*numSecondsPerImage)+(j*50*fftJump):\
+                                  counter+(n*50*numSecondsPerImage)+((j*50*fftJump)+(50*fftWidth)),k+2]
+            curr_fft_secondSensor=scipy.fftpack.fft(currSecondSensor)
+            if "gyro" in firstSensor.lower():
+                finalMatrix[:,j,k,n]=np.abs(curr_fft_secondSensor[:curr_fft_secondSensor.size/2])
+                finalMatrix[:,j,3+k,n]=np.abs(curr_fft_firstSensor[:curr_fft_firstSensor.size/2])
+            else:
+                finalMatrix[:,j,k,n]=np.abs(curr_fft_firstSensor[:curr_fft_firstSensor.size/2])
+                finalMatrix[:,j,3+k,n]=np.abs(curr_fft_secondSensor[:curr_fft_secondSensor.size/2])
 
-
+print(finalMatrix.shape)
+           
 plt.figure(1)
 plt.plot(firstSensor_Time, firstSensor_1, 'r-',firstSensor_Time,firstSensor_2,'g-',firstSensor_Time,firstSensor_3,'b-')
 plt.title(firstSensor+' Signal')
@@ -104,41 +114,77 @@ plt.gcf().set_size_inches(15,5)
 plt.title(secondSensor+' Signal')
 plt.xlabel('Time')
 plt.ylabel('Value')
+
 plt.figure(3)
-plt.plot(x_secondSensor1, np.abs(fft_secondSensor1[:fft_secondSensor1.size/2]), 'r-')
-plt.gcf().set_size_inches(15,5)
-plt.title('FFT of '+secondSensor+' 1')
-plt.xlabel('Frequency')
-plt.ylabel('Value')
-plt.figure(4)
-plt.plot(x_secondSensor2, np.abs(fft_secondSensor2[:fft_secondSensor2.size/2]), 'r-')
-plt.gcf().set_size_inches(15,5)
-plt.title('FFT of '+secondSensor+' 2')
-plt.xlabel('Frequency')
-plt.ylabel('Value')
-plt.figure(5)
-plt.plot(x_secondSensor3, np.abs(fft_secondSensor3[:fft_secondSensor3.size/2]), 'r-')
-plt.gcf().set_size_inches(15,5)
-plt.title('FFT of '+secondSensor+' 3')
-plt.xlabel('Frequency')
-plt.ylabel('Value')
-plt.figure(6)
 plt.plot(x_firstSensor1, np.abs(fft_firstSensor1[:fft_firstSensor1.size/2]), 'r-')
 plt.gcf().set_size_inches(15,5)
 plt.title('FFT of '+firstSensor+' 1')
 plt.xlabel('Frequency')
 plt.ylabel('Value')
-plt.figure(7)
+plt.figure(4)
 plt.plot(x_firstSensor2, np.abs(fft_firstSensor2[:fft_firstSensor2.size/2]), 'r-')
 plt.gcf().set_size_inches(15,5)
 plt.title('FFT of '+firstSensor+' 2')
 plt.xlabel('Frequency')
 plt.ylabel('Value')
-plt.figure(8)
+plt.figure(5)
 plt.plot(x_firstSensor3, np.abs(fft_firstSensor3[:fft_firstSensor3.size/2]), 'r-')
 plt.gcf().set_size_inches(15,5)
 plt.title('FFT of '+firstSensor+' 3')
 plt.xlabel('Frequency')
 plt.ylabel('Value')
+plt.figure(6)
+plt.plot(x_secondSensor1, np.abs(fft_secondSensor1[:fft_secondSensor1.size/2]), 'r-')
+plt.gcf().set_size_inches(15,5)
+plt.title('FFT of '+secondSensor+' 1')
+plt.xlabel('Frequency')
+plt.ylabel('Value')
+plt.figure(7)
+plt.plot(x_secondSensor2, np.abs(fft_secondSensor2[:fft_secondSensor2.size/2]), 'r-')
+plt.gcf().set_size_inches(15,5)
+plt.title('FFT of '+secondSensor+' 2')
+plt.xlabel('Frequency')
+plt.ylabel('Value')
+plt.figure(8)
+plt.plot(x_secondSensor3, np.abs(fft_secondSensor3[:fft_secondSensor3.size/2]), 'r-')
+plt.gcf().set_size_inches(15,5)
+plt.title('FFT of '+secondSensor+' 3')
+plt.xlabel('Frequency')
+plt.ylabel('Value')
+
+for i in range(finalMatrix.shape[3]):
+    plt.rcParams['figure.figsize'] = (15, 6)
+    plt.figure(9+i)
+    plt.subplot(161)
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,0,i])
+    plt.colorbar()
+    plt.subplot(162)
+    plt.xlabel('Time')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,1,i])
+    plt.colorbar()
+    plt.subplot(163)
+    plt.xlabel('Time')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,2,i])
+    plt.colorbar()
+    plt.subplot(164)
+    plt.xlabel('Time')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,3,i])
+    plt.colorbar()
+    plt.subplot(165)
+    plt.xlabel('Time')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,4,i])
+    plt.colorbar()
+    plt.subplot(166)
+    plt.xlabel('Time')
+    plt.yticks( np.arange(5*fftWidth,26*fftWidth,5*fftWidth), (5,10,15,20,25) )
+    plt.imshow(finalMatrix[:,:,5,i])
+    plt.colorbar()
 
 plt.show()
